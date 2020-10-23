cb8dbc50c54e08bae822a64f38fd7c3ec0573f8f
denizduman1 <ce.denizduman@gmail.com>
2020-10-23 14:14:46 +0300

    add

---
+++ AccessModifier/AccessModifier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifier
+{
+    class AccessModifier
+    {
+        static void Main()
+        {
+            //Student student = new Student();
+            //student.ID = 1;
+
+        }
+    }
+
+    //internal class Course  //default class'lar internaldır.Bağlı olduğu projede refarans ihtiyacı olmadan kullanabiliriz.Public class lar tüm projelerde referans eklenip çalışılabilir.
+    //{
+    //    public string Name { get; set; }
+    //}
+
+    //protected class School
+    //{
+    //    public int MyProperty { get; set; }
+    //}
+    //class Customer
+    //{
+    //    public int ID { get; set; }
+    //    private int id; //private blok içerisinde kullanılabilir.
+
+    //    protected string ad; //private özelliklerini karşılar.privatedan farklı kalıtım yaptığı sınıflarda da kullanılabilir.
+    //}
+
+    //class Student:Customer
+    //{
+    //    public void Save()
+    //    {
+    //        ID = 1;
+    //        ad = "0";
+    //    }
+
+    //    //public void Save()
+    //    //{
+    //    //    Customer customer = new Customer();
+    //    //    customer.ID = 1;
+    //    //}
+    //}
+}
